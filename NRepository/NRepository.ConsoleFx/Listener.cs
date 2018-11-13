a421619412796e2d7258ce5584974209fdfa6f6f
Ivan Perez <ivan.perez@nanthealth.com>
2018-11-13 11:33:36 -0500

    Fixed issues with console configuration/setup.

---
+++ NRepository/NRepository.ConsoleFx/Listener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NRepository.MyTestBL.BL;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+
+namespace NRepository.ConsoleFx
+{
+    public class Listener
+    {
+        //This sample doesn't make a constructor where we can pass in the container...
+
+        public Container Container { get; set; }
+        
+        public void HandleMessage(object message)
+        {
+            //Do some message handling work here
+
+            //This requires the container has to be passed in to a listener class (via a property if needed)
+            using (ThreadScopedLifestyle.BeginScope(Container))
+            {
+                var testProvider = Container.GetInstance<TestProvider>();
+
+                var data = testProvider.GetPassingTests();
+            }
+
+            Console.WriteLine("Test Provider loaded successfully");
+        }
+    }
+}
--- NRepository/NRepository.ConsoleFx/Program.cs
+++ NRepository/NRepository.ConsoleFx/Program.cs
@@ -8,22 +8,23 @@ using NRepository.MyTestBL.BL;
 using NRepository.MyTestBL.BL.DataAccess;
 using SimpleInjector;
 using SimpleInjector.Diagnostics;
+using SimpleInjector.Lifestyles;
 
 namespace NRepository.ConsoleFx
 {
     public class Program
     {
         public static Container Container { get; set; }
+        public static Listener Listener { get; set; }
 
         static void Main(string[] args)
         {
             Program.Bootstrap();
 
-            var testProvider = Container.GetInstance<TestProvider>();
+            Listener = new Listener();
+            Listener.Container = Container;
 
-            var data = testProvider.GetPassingTests();
-
-            Console.WriteLine("Test Provider loaded successfully");
+            Listener.HandleMessage("Test Message");
 
             Console.ReadKey();
         }
@@ -31,21 +32,22 @@ namespace NRepository.ConsoleFx
         private static void Bootstrap()
         {
             // 1. Create a new Simple Injector container.
-            var container = new Container();            
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new ThreadScopedLifestyle();
 
             // 2. Configure the container (register)
-            container.Register<ITestRepository, TestRepository>();
-            container.Register<TestProvider>();
-            container.Register<DbContext, MyTestContext>();
+            container.Register<ITestRepository, TestRepository>(Lifestyle.Scoped);
+            container.Register<TestProvider>(Lifestyle.Scoped);
+            container.Register<DbContext, MyTestContext>(Lifestyle.Scoped);
             container.Register<DbContextOptions<MyTestContext>>(() => {
                 return new DbContextOptionsBuilder<MyTestContext>()
                 .UseSqlServer("Server=localhost;Database=TestDB;Trusted_Connection=True;")
                 .Options;
-            }, Lifestyle.Singleton);
+            }, Lifestyle.Scoped);
             
-            Registration registration = container.GetRegistration(typeof(MyTestContext)).Registration;
+            //Registration registration = container.GetRegistration(typeof(MyTestContext)).Registration;
 
-            registration.SuppressDiagnosticWarning(DiagnosticType.DisposableTransientComponent, "Reason of suppression");
+            //registration.SuppressDiagnosticWarning(DiagnosticType.DisposableTransientComponent, "Reason of suppression");
 
             // 3. Store the container for use by Page classes.
             Container = container;
