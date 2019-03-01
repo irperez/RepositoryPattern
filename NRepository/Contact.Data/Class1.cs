c8473add3a0f174f92ed44d47549523e637792f1
Bob Godfrey <bob_godfrey@hotmail.com>
2019-03-01 16:11:55 -0500

    asdf

---
+++ NRepository/Contact.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Contact.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ NRepository/Contact.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Contact.Domain
+{
+    public class Class1
+    {
+    }
+}
